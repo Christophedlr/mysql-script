2ef3babcbc8e1e7ff70009f9fd032d66fd5d9a58
Christophe Daloz - De Los Rios <christophedlr@gmail.com>
2021-05-18 10:13:17 +0200

    DatabaseCommand
    - Add delete database
    - Add revoke all privileges for database in user
    
    Signed-off-by: Christophe Daloz - De Los Rios <christophedlr@gmail.com>

--- mysql/database.py
+++ mysql/database.py
@@ -14,6 +14,9 @@ class DatabaseCommand(Command):
     user: str
     
     def __init__(self):
+        """
+        Initialization
+        """
         super(DatabaseCommand, self).__init__()
 
         self.root = ''
@@ -29,6 +32,10 @@ class DatabaseCommand(Command):
         self.add_argument('-d', 1, str, 'Delete a database name')
 
     def fill(self, args: dict):
+        """
+        Fill properties with the used arguments
+        :param args: arguments
+        """
         if args['p']:
             self.root = args['p']
 
@@ -44,24 +51,45 @@ class DatabaseCommand(Command):
 
         self.fill(args)
 
+        # If root password is not specified in command line
         if not self.root:
             simple = SimpleQuestion()
             self.root = simple.run('Password of root MySQL/MariaDB user', True)
 
+        # If create a new database
         if self.create:
             sql = "CREATE DATABASE IF NOT EXISTS {0};\n".format(self.create)
 
             question = YesNoQuestion()
             if question.run('Grant all privileges for an user ?', True):
                 simple = SimpleQuestion()
-                user = simple.run('Name of a valid user :')
+                self.user = simple.run('Name of a valid user :')
 
-                sql += "GRANT ALL PRIVILEGES ON {0}.* TO '{1}'@localhost;\n".format(self.create, user)
+                sql += "GRANT ALL PRIVILEGES ON {0}.* TO '{1}'@localhost;\n".format(self.create, self.user)
             sql += "FLUSH PRIVILEGES;"
 
             os.system('mysql -u root --password={0} -e "{1}"'.format(self.root, sql))
 
             print("The {0} database has been created.".format(self.create))
 
-            if user:
-                print("The privileges of {0} for {1} database has been updated.".format(user, self.create))
+            if self.user:
+                print("The privileges of {0} for {1} database has been updated.".format(self.user, self.create))
+
+        # Else if delete exist database
+        elif self.delete:
+            sql = "DROP DATABASE IF EXISTS {0};\n".format(self.delete)
+
+            question = YesNoQuestion()
+            if question.run("Revoke all privileges for an user ?", True):
+                simple = SimpleQuestion()
+                self.user = simple.run("Name of a valid user :")
+
+                sql += "REVOKE ALL PRIVILEGES ON {0}.* FROM '{1}'@localhost;\n".format(self.delete, self.user)
+            sql += "FLUSH PRIVILEGES;"
+
+            os.system('mysql -u root --password={0} -e "{1}"'.format(self.root, sql))
+
+            print("The {0} database has been deleted".format(self.delete))
+
+            if self.user:
+                print("The privileges of {0} for {1} database has been revoked.".format(self.user, self.delete))
